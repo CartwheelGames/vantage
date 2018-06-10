b9b1352640016b21da60185900f49ca0d4905f29
sugar3900 <ksugar@tresensa.com>
2018-06-10 13:40:08 -0400

    Player Raycasting

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour 
+{
+
+	public Camera camera;
+
+	private void Start () 
+	{
+
+		
+	}
+	
+
+	private void Update () 
+	{
+
+		Enemy enemy = GetEnemyHitting();
+
+		Debug.Log(enemy);
+
+	}
+
+	private Enemy GetEnemyHitting () 
+	{
+		RaycastHit hit;
+
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        
+        Physics.Raycast(ray, out hit);
+
+        if (hit.collider != null)
+        {
+        	GameObject hitObject = hit.collider.gameObject;
+
+            if (IsEnemy(hitObject))
+            {
+            	return hitObject.GetComponent<Enemy>();
+            }
+
+        }
+
+        return null;
+	}
+
+	private bool IsEnemy (GameObject target)
+	{
+		return target != null && target.GetComponent<Enemy>() != null;
+	}
+}
