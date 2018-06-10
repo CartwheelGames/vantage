9c9a8c9e68ea75ebe1d403bea6d49bfcc5bfb654
Michael Consoli <michael@cartwheelgames.com>
2018-06-10 12:39:14 -0400

    Using c# 4

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private int HealthPoints { get; }
+	private void Start ()
+    {
+        
+	}
+
+	private void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/Enemies/EnemySpawnPoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnPoint : MonoBehaviour
+{
+    [SerializeField]
+    private float radius = 0f;
+    public float Radius
+    {
+        get
+        {
+            return Radius;
+        }
+    }
+}
