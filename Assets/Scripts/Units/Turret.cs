ff219aa1fa76f694a2ef451c476da58bb1a7625d
sugar3900 <ksugar@tresensa.com>
2018-06-12 19:16:10 -0400

    new inherited classes, turret instance

---
+++ Assets/Scripts/Units/Turret.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Units
+{
+	public class Turret : UnitStatic 
+	{
+		void Start () 
+		{
+		
+		}
+	
+
+	}
+}
