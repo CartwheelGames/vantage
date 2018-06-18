7d17802326686c5c9eaae20ef00f0a009a044492
sugar3900 <ksugar@tresensa.com>
2018-06-18 19:22:09 -0400

    added linked pathNodes

---
+++ Assets/Scripts/PathNode.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode : MonoBehaviour {
+
+	[SerializeField]
+	PathNode nextNode = null;
+
+	GameObject GetNextNode () 
+	{
+		return nextNode.gameObject;
+	}
+}
